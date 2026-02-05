e559581b8172ae5cde5810687978fdf91ece9635
Utso Chaudhuri <utso.chaudhuri@gmail.com>
2026-02-05 21:24:06 +0000

    tested new strategies to implement thomas algo

--- OneDrive/Desktop/bsm_pde_num/test.py
+++ OneDrive/Desktop/bsm_pde_num/test.py
@@ -1,25 +1,87 @@
 import numpy as np
 import math
-r   = 0.05
-vol = 0.2
-ds  = 10
-dt  = 0.005
-K   = 100
-def Calc_Ai(vol,r,i):
-    return 0.5*(vol**2)*i**2 - 0.5*r*i
-def Calc_Bi(vol,r,i):
-    return -((vol**2)*(i**2)+r)
-def Calc_Ci(vol,r,i):
-    return 0.5*(vol**2)*(i**2)+0.5*r*i
 
-vec_vn = [3,4,5,6]
+r=0.05
+vol=0.2
+ds=10
+dt=0.005
+K=100
 
-def build_d_1st_step():
-    vec_d = [(1-dt/2*Calc_Bi(vol,r,1)*vec_vn[0])+(-dt/2*Calc_Ci(vol,r,1)*vec_vn[1])]
-    for i in range(len(vec_vn)-2):
-        val = (-dt/2*Calc_Ai(vol,r,i+2)*vec_vn[i])+(1-dt/2*Calc_Bi(vol,r,i+2)*vec_vn[i+1])+(-dt/2*Calc_Ci(vol,r,i+2)*vec_vn[i+2])
-        vec_d.append(val)
-    vec_d.append((-dt/2*Calc_Ai(vol,r,len(vec_vn))*vec_vn[-2])+(1-dt/2*Calc_Bi(vol,r,len(vec_vn))*vec_vn[-1]))
-    vec_d = np.array(vec_d, dtype=float)
-    return vec_d
+def Calc_Ai(dt,vol,r,i):
+    return -0.5*dt*((vol**2)*(i**2)-r*i)
 
+def Calc_Bi(dt,vol,r,i):
+    return 1+dt*((vol**2)*(i**2)+r)
+
+def Calc_Ci(dt,vol,r,i):
+    return -0.5*dt*((vol**2)*(i**2)+r*i)
+
+Smin = 0
+Smax = 400
+
+def payoff_call(S):
+    return max(S-K, 0.0)
+
+def call_boundary(Smax, K, r, tau):
+    return Smax - K * math.exp(-r * tau)
+
+def build_b_1st_step():
+    tau = dt
+    vec_b = []
+    for S in range(Smin+ds, Smax, ds):  # interior nodes only: 10..390
+        vec_b.append(payoff_call(S))
+    # boundary correction applied to LAST interior node (Smax-ds -> i = M-1)
+    V_boundary = call_boundary(Smax, K, r, tau)
+    M = int(Smax/ds)
+    i_last = M - 1
+    vec_b[-1] = vec_b[-1] - Calc_Ci(dt, vol, r, i_last) * V_boundary
+    return np.array(vec_b, dtype=float)
+
+def thomas_algo(times):
+    # Initial RHS from payoff at maturity
+    vec_b = build_b_1st_step()
+
+    M = int(Smax/ds)
+    # Interior unknown indices: i = 1..M-1  (matches vec_b length)
+    j = list(range(1, M))  # length M-1
+
+    for step in range(times):
+        N = len(vec_b)
+
+        # --- Forward sweep ---
+        c_dash = np.zeros(N, dtype=float)
+        d_dash = np.zeros(N, dtype=float)
+
+        c_dash[0] = Calc_Ci(dt, vol, r, j[0]) / Calc_Bi(dt, vol, r, j[0])
+        d_dash[0] = vec_b[0] / Calc_Bi(dt, vol, r, j[0])
+
+        for k in range(1, N):
+            idx = j[k]
+            den = Calc_Bi(dt, vol, r, idx) - Calc_Ai(dt, vol, r, idx) * c_dash[k-1]
+            c_dash[k] = Calc_Ci(dt, vol, r, idx) / den
+            d_dash[k] = (vec_b[k] - Calc_Ai(dt, vol, r, idx) * d_dash[k-1]) / den
+
+        # --- Back substitution ---
+        x = np.zeros(N, dtype=float)
+        x[-1] = d_dash[-1]
+        for k in range(N-2, -1, -1):
+            x[k] = d_dash[k] - c_dash[k] * x[k+1]
+
+        # If another step remains, rebuild RHS for next solve:
+        # vec_b := V^n (current known layer) then apply boundary correction to last entry
+        if step != times - 1:
+            tau = (step + 2) * dt  # next step uses tau = 2dt, 3dt, ...
+            vec_b = x.copy()
+            V_boundary = call_boundary(Smax, K, r, tau)
+            i_last = M - 1
+            vec_b[-1] = vec_b[-1] - Calc_Ci(dt, vol, r, i_last) * V_boundary
+        else:
+            vec_b = x.copy()
+
+    vec_b = [round(val,4) for val in vec_b]
+    return vec_b
+
+output = thomas_algo(2)
+a = [num for num in range(10,410,10)]
+res = dict(zip(a,output))
+print(res)
